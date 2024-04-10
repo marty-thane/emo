4735736215b1c2e4685b0f64baa6fd6c01218b3a
Marty Thane <mzf@tuta.io>
2024-04-10 20:32:26 +0200

    redone

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -1,57 +1,101 @@
-# Solving the knapsack problem using blind algorithm. Loads an external JSON
-# file. Uses built-in python types like dict and list, which may be slow for
-# large numbers of items.
-
 import json
-from random import randint
+import numpy as np
+from time import time
 
-JSON_FILE = "items.json"
-GENERATIONS = 1000
-MAX_WEIGHT = 10
+class Item:
+    def __init__(self, name: str, value: int, weight: float):
+        self._name = name
+        self._value = value
+        self._weight = weight
 
-def load(file: str) -> dict:
-    """
-    Loads a JSON file provided as an argument and returns it as a dictionary.
-    """
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def value(self) -> int:
+        return self._value
+
+    @property
+    def weight(self) -> float:
+        return self._weight
+
+def parse_json(file: str) -> dict:
     with open(file, "r") as f: return json.load(f)
 
-def generate_from(items: dict) -> list:
-    """
-    Returns a random genotype based on items provided as an argument.
-    """
-    return [randint(0, 1) for _ in range(len(items))]
+def itemize(data: dict) -> list:
+    items = []
+    for item_name, item_data in data.items():
+        item = Item(name=item_name, value=item_data['value'], weight=item_data['weight'])
+        items.append(item)
+    return items
 
-def gamma_of(genotype: list, items: dict) -> dict:
-    """
-    Filters the items by a given genotype. Returns the filtered dict.
-    """
-    filtered_keys = [i for i, g in zip(items, genotype) if g]
-    filtered_items = {key: items[key] for key in filtered_keys}
-    return filtered_items
+def gamma(genotype: np.ndarray) -> list:
+    return [i for g, i in zip(genotype, ITEMS) if g]
 
-def fitness_of(fenotype: dict) -> int:
-    """
-    Calculates fitness of a particular fenotype, which it returns.
-    """
-    total_weight = sum([item["weight"] for item in fenotype.values()])
+def fitness(phenotype: list) -> int:
+    total_weight = sum([item.weight for item in phenotype])
     if total_weight > MAX_WEIGHT:
-        return 0
-
-    total_value = sum([item["value"] for item in fenotype.values()])
+        return 1
+    total_value = sum([item.value for item in phenotype])
     return total_value
 
-def main():
-    best_fenotype = {}
-    best_fitness = 0
+def select(genotypes: np.ndarray, scores: np.ndarray) -> np.ndarray:
+    random_number = np.random.rand()
+    cumulative_score = 0
+    for idx, score in enumerate(scores):
+        cumulative_score += score
+        if cumulative_score >= random_number:
+            return genotypes[idx]
 
-    items = load(JSON_FILE)
+def crossover(parent1: np.ndarray, parent2: np.ndarray) -> list[np.ndarray, np.ndarray]:
+    splitpoint = np.random.randint(0, len(parent1))
+    child1 = np.concatenate((parent1[:splitpoint], parent2[splitpoint:]))
+    child2 = np.concatenate((parent2[:splitpoint], parent1[splitpoint:]))
+    return child1, child2
+
+def mutate(genotype: np.ndarray) -> np.ndarray:
+    mut = np.copy(genotype) # do not modify original
+    for idx in range(len(genotype)):
+        if np.random.rand() <= MUTATION_RATE:
+            mut[idx] = 1 - mut[idx]
+    return mut
+
+JSON_FILE = "items.json"
+MAX_WEIGHT = 10
+POPULATION_SIZE = 100
+GENERATIONS = 50
+MUTATION_RATE = 0.05
+ITEMS = itemize(parse_json(JSON_FILE))
+
+def main():
+    print(f"found {len(ITEMS)} items, weight limit {MAX_WEIGHT}")
+    print(f"{2**len(ITEMS)} possible solutions, will try {POPULATION_SIZE*GENERATIONS}")
+
+    start_time = time()
+
+    genotypes = np.random.randint(0, 2, (POPULATION_SIZE, len(ITEMS)))
+
+    best_phenotype = []
+    best_score = 0
     for _ in range(GENERATIONS):
-        genotype = generate_from(items)
-        fenotype = gamma_of(genotype, items)
-        fitness = fitness_of(fenotype)
-        if fitness > best_fitness:
-            best_fenotype = fenotype
-            best_fitness = fitness
-            print(f"items: {list(best_fenotype)}, value: {best_fitness}")
+        phenotypes = [gamma(g) for g in genotypes]
+        scores = np.array([fitness(f) for f in phenotypes])
+
+        best_idx = np.argmax(scores)
+        if scores[best_idx] > best_score:
+            best_phenotype = phenotypes[best_idx]
+            best_score = scores[best_idx]
+
+        scores = scores / np.sum(scores) # normalize
+        genotypes = np.array([select(genotypes, scores) for _ in range(POPULATION_SIZE)])
+        genotypes = np.reshape([crossover(genotypes[idx], genotypes[idx+1]) for idx in range(0, POPULATION_SIZE, 2)], (POPULATION_SIZE, len(ITEMS)))
+        genotypes = np.array([mutate(g) for g in genotypes])
+
+    stop_time = time()
+
+    print(f"found soution with score {best_score}:")
+    print(np.array([item.name for item in best_phenotype]))
+    print(f"finished in {round(stop_time-start_time, 2)}s")
 
 main()
